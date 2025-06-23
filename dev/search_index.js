f9da62a54822fc8c5c9248ea2a9540fc9ac781c4
Documenter.jl <documenter@juliadocs.github.io>
2025-06-23 06:53:48 +0000

    build based on 230e487

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#SunlightHNC","page":"Home","title":"SunlightHNC","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SunlightHNC.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
